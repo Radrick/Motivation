05d48e13ad79bbe5a268658b8b1d011f68b159b7
Radrick Gomes <radrickgomes2708@hotmail.com>
2020-06-05 13:54:48 -0300

    instance Shared preferences; created package infra and ui

--- app/src/main/java/com/example/motivation/ui/SplashscreenActivity.kt
+++ app/src/main/java/com/example/motivation/ui/SplashscreenActivity.kt
@@ -1,16 +1,24 @@
-package com.example.motivation
+package com.example.motivation.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splashscreen.*
 
 class SplashscreenActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        mSecurityPreferences =  SecurityPreferences(this)
 
         if (supportActionBar != null) {
             supportActionBar!!.hide()
@@ -26,10 +34,15 @@ class SplashscreenActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun handleSave(){
-        if(fText_name.text.toString().isNotEmpty()){
+
+        var name = fText_name.text.toString()
+
+        if(name.isNotEmpty()){
+            mSecurityPreferences.storeString("name", name)
             startActivity(Intent(this, MainActivity::class.java))
         } else {
-            Toast.makeText(this, R.string.empty_field_name, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,
+                R.string.empty_field_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
